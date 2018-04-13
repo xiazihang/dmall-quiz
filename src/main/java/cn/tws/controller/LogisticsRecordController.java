2d9cc453d03ebdb142c3233891f5759d077a17de
DH-TB <929659475@qq.com>
2018-04-13 12:22:56 +0800

    fix LogisticsRecord restful
    Signed-off-by: DH-TB <929659475@qq.com>

--- src/main/java/cn/tws/controller/LogisticsRecordController.java
+++ src/main/java/cn/tws/controller/LogisticsRecordController.java
@@ -2,37 +2,38 @@ package cn.tws.controller;
 
 import cn.tws.entity.LogisticsRecord;
 import cn.tws.repository.LogisticsRecordRepository;
+import cn.tws.utils.LogisticsStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/logisticsRecord")
 public class LogisticsRecordController {
 
     @Autowired
     private LogisticsRecordRepository logisticsRecordRepository;
 
-    @PostMapping("/logisticsRecord")
+    @PostMapping("")
     public ResponseEntity createRecordRepository(@RequestBody LogisticsRecord logisticsRecord) throws Exception {
         logisticsRecordRepository.save(logisticsRecord);
         LogisticsRecord logisticsRecords = logisticsRecordRepository.findOne(logisticsRecord.getId());
         return new ResponseEntity<>(logisticsRecords,HttpStatus.CREATED);
     }
 
-    @GetMapping(value = "/logisticsRecord/{id}")
+    @GetMapping(value = "/{id}")
     public ResponseEntity getRecordRepository(@PathVariable Long id) throws Exception {
         LogisticsRecord logisticsRecord = logisticsRecordRepository.findOne(id);
         return new ResponseEntity<>(logisticsRecord, HttpStatus.OK);
     }
 
-    @PutMapping(value = "/logisticsRecord/{id}")
+    @PutMapping(value = "/{id}")
     public ResponseEntity shipping(@PathVariable Long id, @RequestBody LogisticsRecord logisticsRecord) throws Exception {
         LogisticsRecord oldLogisticsRecord = logisticsRecordRepository.findOne(id);
         oldLogisticsRecord.setOutboundTime(logisticsRecord.getOutboundTime());
         oldLogisticsRecord.setSignedTime(logisticsRecord.getSignedTime());
-        oldLogisticsRecord.setLogisticsStatus((LogisticsRecord.LogisticsStatus) logisticsRecord.getLogisticsStatus());
+        oldLogisticsRecord.setLogisticsStatus((LogisticsStatus) logisticsRecord.getLogisticsStatus());
         logisticsRecordRepository.save(oldLogisticsRecord);
         return new ResponseEntity<>(logisticsRecordRepository.findOne(id), HttpStatus.OK);
     }
---
+++ src/main/java/cn/tws/utils/LogisticsStatus.java
@@ -0,0 +1,7 @@
+package cn.tws.utils;
+
+public enum LogisticsStatus {
+    readyToShip,
+    shipping,
+    signed
+}
